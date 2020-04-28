63230e38fb1dd4fc62e98e4cc8de659b088a2f95
Hu Xu <activebus@gmail.com>
2020-04-28 12:24:05 -0400

    add e2e.

--- transformers/script/run_config.py
+++ transformers/script/run_config.py
@@ -8,19 +8,24 @@ trainconfig = TrainConfig()
 taskconfigs = [
     TaskConfig("ae", "14", "laptop"),
     TaskConfig("asc", "14", "laptop"),
-    # TaskConfig("e2e", "14", "laptop"),
+    TaskConfig("e2e", "14", "laptop"),
     
     TaskConfig("ae", "16", "rest"),
     TaskConfig("asc", "14", "rest"),
-    # TaskConfig("e2e", "union", "rest"),
+    TaskConfig("e2e", "union", "rest"),
 ]
 
 baselines = [
     BaselineConfig("BERT", model_name_or_path="bert-base-uncased"),
+    BaselineConfig("BERT-DK", model_name_or_path="activebus/BERT-DK_[DOMAIN]"),
+    BaselineConfig("BERT-PT", model_name_or_path="activebus/BERT-PT_[DOMAIN]"),
+    
     BaselineConfig("BERT_Review", model_name_or_path="activebus/BERT_Review"),
     # BaselineConfig("Bert_Review", model_name_or_path="pt_runs/pt_bert-Bert"),
     BaselineConfig("BERT-XD_Review", model_name_or_path="activebus/BERT-XD_Review"),
     # BaselineConfig("BERT-XD_Review", model_name_or_path="pt_runs/pt_bertselect-SkipDomBert"),
+    
+    
 ]
 
 
@@ -31,6 +36,7 @@ if __name__=="__main__":
             config.update(trainconfig.to_dict())
             config.update(taskconfig.to_dict())
             config.update(baseline.to_dict())
+            config["model_name_or_path"] = config["model_name_or_path"].replace("[DOMAIN]", config["domain"])
             config_name = "_".join([config["task"], config["domain"], config["year"], config["baseline"]])
             config["output_dir"] = os.path.join("ft_runs", config_name)
 
@@ -40,5 +46,4 @@ if __name__=="__main__":
                 json.dump(config, fw)
             
             with open("configs/" + config_name + ".json", "w") as fw:
-                json.dump(config, fw)
-                
+                json.dump(config, fw)            
